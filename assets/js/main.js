f3435d64830b04683b6ce2f99754a8fe16b97159
mauriciocoruja <mbjr@outlook.com.br>
2019-06-25 21:59:45 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+let ulTarefas = document.querySelector('#aFazer')
+
+const listarTarefas = (elementoHtml) => {
+    fetch('http://localhost:8000/api/listar-tarefas')
+        .then(resposta => resposta.json())
+        .then(dados => {
+            let html =''
+            dados.forEach(dado => {
+                html += `
+                    <li class="list-group-item d-flex">
+                        <span>${dado.conteudo}</span>
+                        <div class="ml-auto">
+                            <a id="ConcluirTarefa" tarefa-id=${dado.id} href="#">
+                                <i title="Concluir tarefa" class="material-icons">done</i>
+                            </a>
+                            <a id="deletarTarefa" tarefa-id=${dado.id} href="#">
+                                <i title="Deletar tarefa" class="material-icons">delete</i>
+                            </a>
+                        </div>
+                    </li>
+                `
+            })
+            elementoHtml.innerHTML = html
+        })
+}
+
+// listarTarefas(ulTarefas)
+
+// adicionarTarefa = (tarefa) => {
+//     let dados = new FormData()
+//     dados.append('conteudo', tarefa)
+        
+// ​
+//     fetch('http://localhost:8000/api/adicionar-tarefa', {
+//         method: 'post',
+//         body: dados
+//     }).then(resposta => resposta.json()).then(dados =console.log(dados)) 
+// }
